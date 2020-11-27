6df536d74a632faa689b9a3eab937168798d33bc
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2020-11-27 18:05:58 -0500

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 import './styles/Navbar.css';
 import logo from '../images/logo.svg';
@@ -9,11 +9,11 @@ class Navbar extends React.Component {
     return (
       <div className="Navbar">
         <div className="container-fluid">
-          <Link className="Navbar__brand" to="/">
+          <a className="Navbar__brand" to="/">
             <img className="Navbar__brand-logo" src={logo} alt="Logo" />
             <span className="font-weight-light">Platzi</span>
             <span className="font-weight-bold">Conf</span>
-          </Link>
+          </a>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
 ReactDOM.render(
-    <Badge
-        firstName="Héctor"
-        lastName="Moreira"
-        avatarUrl="https://static.platzi.com/media/avatars/avatars/_hhugom_06e4ec41-e943-43a3-87ba-3f8b894c4d0c.jpg"
-        jobTitle="Web Developer"
-        twitter="hectormoreira"
-    />,
-    container
+    <BadgeNew />,container
 );
