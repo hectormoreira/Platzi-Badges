c7a63665e7be4e7e6e9203665c5e22fb4de99f23
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2020-11-27 16:59:35 -0500

    Add styles

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Platzi-Badges
 Course React.js - Platzi: PlatziBadges
+
+## Global react
+```
+npm install -g create-react-app
+```
+
+## Run server
+```
+npm run start
+```
+
+## Install Bootstrap
+```
+npm install bootstrap
+```
--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,31 +1,31 @@
 import React from 'react';
+import "./styles/Badge.css";
 
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://static.platzi.com/media/avatars/avatars/_hhugom_06e4ec41-e943-43a3-87ba-3f8b894c4d0c.jpg" alt="Avatar" />
           <h1>
-            Richard <br /> Kaufman
+            Héctor <br /> Moreira
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@sparragus</p>
+        <div className="Badge__section-info">
+          <h3>Web Developer</h3>
+          <div>@hectomoreira</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
 }
-
 export default Badge;
