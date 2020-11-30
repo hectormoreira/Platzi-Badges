607eae82aec200c1519549407e2fb08a7f126c1d
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2020-11-30 16:42:57 -0500

    working with states

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 class BadgeForm extends Component {
+  //state = {};
 
-  handleChange = (e) => {
-    console.log({
-      name: e.target.name,
-      value: e.target.value
-    });
-  }
+  // handleChange = (e) => {
+  //   // console.log({
+  //   //   name: e.target.name,
+  //   //   value: e.target.value
+  //   // });
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
 
   handleClick = (e) => {
     console.log("Click");
@@ -16,12 +20,8 @@ class BadgeForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("from handleSubmit");
-
-
+    console.log(this.state);
   }
-
-
-
 
   render() {
     return (
@@ -30,8 +30,58 @@ class BadgeForm extends Component {
 
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <label>Firs Name</label>
-            <input onChange={this.handleChange} className="form-control" type="text" name="firstName" />
+            <label>First Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+              value={this.props.formValues.firstName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.props.formValues.lastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.props.formValues.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Job title</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.props.formValues.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Twitter</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.props.formValues.twitter}
+            />
           </div>
 
           <button onClick={this.handleClick} className="btn btn-primary">Save</button>
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -6,6 +6,26 @@ import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends Component {
+  state = {
+    form: {
+      firstName:'',
+      lastName:'',
+      email:'',
+      jobTitle:'',
+      twitter:''
+    }
+  }
+
+  handleChange= (e)=>{
+    this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    })
+  }
+
+
   render() {
     return (
       <div>
@@ -18,15 +38,16 @@ class BadgeNew extends Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Hector"
-                lastName="Moreira"
-                twitter="hectormoreira"
-                jobTitle="Web Developer"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://static.platzi.com/media/avatars/avatars/_hhugom_06e4ec41-e943-43a3-87ba-3f8b894c4d0c.jpg"
               />
             </div>
             <div className="col-6">
-              <BadgeForm/>
+              <BadgeForm onChange={this.handleChange} formValues={this.state.form} />
             </div>
           </div>
         </div>
