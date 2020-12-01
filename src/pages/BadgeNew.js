21bf57ce304cc35f91ac8429cce5f64c455df716
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2020-12-01 18:05:17 -0500

    dev gravatar

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./styles/BadgeNew.css"
-import header from '../images/badge-header.svg'
+import header from '../images/platziconf-logo.svg'
 import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
@@ -30,16 +30,16 @@ class BadgeNew extends Component {
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
-          <img className="img-fluid" src={header} alt="Logo" />
+          <img className="img-fluid BadgeNew__hero-image" src={header} alt="Logo" />
         </div>
         <div className="container">
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName={this.state.form.firstName}
-                lastName={this.state.form.lastName}
-                twitter={this.state.form.twitter}
-                jobTitle={this.state.form.jobTitle}
+                firstName={this.state.form.firstName || 'First_Name'}
+                lastName={this.state.form.lastName || 'Last_Name'}
+                twitter={this.state.form.twitter || 'Twitter'}
+                jobTitle={this.state.form.jobTitle || 'Job_Title'}
                 email={this.state.form.email}
                 avatarUrl="https://static.platzi.com/media/avatars/avatars/_hhugom_06e4ec41-e943-43a3-87ba-3f8b894c4d0c.jpg"
               />
