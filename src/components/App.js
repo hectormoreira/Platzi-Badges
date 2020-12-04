229d8a15c29a034379a7726403ec7a76dedaf8a4
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2020-12-04 15:03:58 -0500

    ui components and ui container

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Layout from "./Layout"
 import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
 import BadgeEdit from "../pages/BadgeEdit";
-import BadgeDetails from "../pages/BadgeDetails";
+import BadgeDetailsContainer from "../pages/BadgeDetailsContainer";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
 
@@ -17,7 +17,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
-          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetailsContainer} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
